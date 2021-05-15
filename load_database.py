a8610c4526ac876800b8c76b43330d100b30b225
kyselica12 <dano.kyselica@gmail.com>
2021-05-15 10:49:41 +0200

    Unfinished games saved

--- load_database.py
+++ load_database.py
@@ -1,9 +1,21 @@
 from game import *
 import glob
 
+def check_moves_short(c_moves, winner):
+    return c_moves < 9  # too short game someone has resign
+
+def check_moves_draw(c_moves, winner):
+    return winner == 0
+
+def check_moves_unfinished(c_moves, winner):
+    if winner == 1 and c_moves % 2 == 0:  # wrong number of moves
+        return True
+    if winner == -1 and c_moves % 2 == 1:  # wrong number of moves
+        return True
+    return False
 
 def check_moves(c_moves, winner):
-    if c_moves < 9:  # too short game someone has resign
+    if c_moves < 9:
         return False
     if winner == 0:  # draw
         return False
@@ -15,7 +27,7 @@ def check_moves(c_moves, winner):
 
 
 def interpret_moves(moves):
-    return tuple((int(moves[m], 16) - 1, int(moves[m+1], 16) - 1) for m in range(0,len(moves),2))
+    return tuple((int(moves[m], 16) - 1, int(moves[m + 1], 16) - 1) for m in range(0, len(moves), 2))
 
 
 def load_file(path):
@@ -24,10 +36,11 @@ def load_file(path):
         data = f.read()
     lines = data.splitlines()
     game = Game()
-    states = []
-    ok = 0
-    bad_moves = 0
-    bad_unfinished = 0
+    ok_games = []
+    unfinished_games = []
+
+    stats = {'ok':0, 'draw':0, 'unfinished':0, 'short':0}
+
     for line in data.splitlines():
         winner_str, moves = line.split(',')[3:5]
 
@@ -39,27 +52,41 @@ def load_file(path):
 
         # we dont want to store these games
         c_moves = len(moves) // 2
-        if not check_moves(c_moves, winner):
-            bad_moves += 1
-            continue
 
-        moves = interpret_moves(moves)
+        if check_moves_short(c_moves, winner):
+            stats['short'] += 1
 
-        s = GameState(moves[:-1], on_turn=winner)
-        s = game.move(s, moves[-1])
-        if not s.terminal:
-            bad_unfinished += 1
-            continue
-        ok += 1
-        states.append(s.__dict__)
-    print(f'Total games {len(data.splitlines())}, OK: {ok}, BAD:{bad_moves, bad_unfinished}')
-    return states
+        elif check_moves_draw(c_moves, winner):
+            stats['draw'] += 1
+
+        elif check_moves_unfinished(c_moves, winner):
+            stats['unfinished'] += 1
+            unfinished_games.append(s.__dict__)
+        else:
+            moves = interpret_moves(moves)
+            s = GameState(moves[:-1], on_turn=winner)
+            s = game.move(s, moves[-1])
+            if not s.terminal:
+                stats['unfinished'] += 1
+                unfinished_games.append(s.__dict__)
+            else:
+                stats['ok'] += 1
+                ok_games.append(s.__dict__)
+
+    print(f'Total games {len(data.splitlines())}, {stats}')
+    return ok_games, unfinished_games
+
 
 if __name__ == '__main__':
-    states = []
+    ok, unfinished = [], []
     for path in glob.iglob("game_databases/*.bdt"):
         print(path)
-        states += load_file(path)
+        tmp = load_file(path)
+        ok += tmp[0]
+        unfinished += tmp[1]
 
-    with open('games_history.json', 'w') as f:
-        json.dump(states, fp=f)
+    with open('ok.json', 'w') as f:
+        json.dump(ok, fp=f)
+
+    with open('unfinished.json' ,'w') as f:
+        json.dump(unfinished, fp=f)
